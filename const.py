7f021f3bdbbb5620bc612bcd819264325cde5f5b
Rahul K <rahulkmr.kmr75@gmail.com>
2016-10-26 17:13:43 +0530

    made constant py

--- const.py
+++ const.py
@@ -1,1 +1,15 @@
+#stores the value of constants
+'''the first index is low_val the next is 
+high_val'''
+mybotcol1=[[0,0,0],[180,255,255]]
+mybotcol2=[[0,0,0],[180,255,255]]
+
+opbotcol1=[[0,0,0],[180,255,255]]
+opbotcol2=[[0,0,0],[180,255,255]]
+
+ball=[[0,0,0],[180,255,255]]
+
+#the pid constants
+klinear=0
+kangular=0
 
